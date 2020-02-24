e7584cbebe339216c0bc340642cfd4377aab7b6d
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-02-24 20:50:59 +0000

    first commit for Nested if statements tutorial

---
+++ src/chapter3/LoanQualifier.java
@@ -0,0 +1,8 @@
+package chapter3;
+/*
+ * Nested Ifs:
+ * To qualify for a loan, a person must make at least £30,000
+ * and have been working at their current job for at least 2 years.
+ */
+public class LoanQualifier {
+}
