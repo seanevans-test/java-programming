3efb743ce62792d531341d84e4205f943863b391
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-02-25 18:18:51 +0000

    Added the test score entry allowing decimal number entry through double

--- src/chapter3/TestResults.java
+++ src/chapter3/TestResults.java
@@ -1,10 +1,15 @@
 package chapter3;
 
+import java.util.Scanner;
+
 public class TestResults {
 
     public static void main (String args[]){
 
         //Get the test score
+        System.out.println("Enter your test score here:");
+        Scanner scanner = new Scanner(System.in);
+        double score = scanner.nextDouble();
 
         //Determine the letter grade
 
