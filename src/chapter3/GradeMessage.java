ec3824c27832246379d859e69479088f2a10611e
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-02-26 21:43:04 +0000

    first commit for Switch Statements class

---
+++ src/chapter3/GradeMessage.java
@@ -0,0 +1,14 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class GradeMessage {
+
+    public static void main(String args[]){
+
+        System.out.println("Enter your letter grade:");
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
