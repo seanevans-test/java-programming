aec926d0a42701dba471e66dcb00818ca11761a1
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-03-02 21:00:10 +0000

    set out game, added option for inputting numOfPennies option

--- src/chapter3/ChangeForADollar.java
+++ src/chapter3/ChangeForADollar.java
@@ -1,5 +1,7 @@
 package chapter3;
 
+import java.util.Scanner;
+
 public class ChangeForADollar {
 
     public static void main(String args[]){
@@ -10,5 +12,11 @@ public class ChangeForADollar {
         double quarter = .25;
         int dollar = 1;
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to 'change for a Dollar'! " + "Your goal is to make exactly $1.00");
+
+        System.out.println("Enter your number of pennies");
+        int numOfPennies = scanner.nextInt();
     }
 }
