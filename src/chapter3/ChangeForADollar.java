bce3051d0da3cb25d37661cdc87468172e5ea459
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-03-02 20:53:31 +0000

    first commit for ChangeForADollar game, set out parameters using double and it

---
+++ src/chapter3/ChangeForADollar.java
@@ -0,0 +1,14 @@
+package chapter3;
+
+public class ChangeForADollar {
+
+    public static void main(String args[]){
+
+        double penny = .01;
+        double nickel = .05;
+        double dime = .10;
+        double quarter = .25;
+        int dollar = 1;
+
+    }
+}
