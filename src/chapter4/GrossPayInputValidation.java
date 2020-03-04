a8c9c3b23f19ae2bff8f798d11787bbdfef0551e
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-03-04 20:47:31 +0000

    first commit on while loop tutorial

---
+++ src/chapter4/GrossPayInputValidation.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+/*
+ *WHILE LOOP:
+ * Each store employee makes £15 an hour. Write a program that allows the employee
+ *  to enter the number of hours worked for the week. Do not allow overtime.
+ */
+
+public class GrossPayInputValidation {
+    public static void main(String args[]){
+
+
+    }
+}
