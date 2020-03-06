51ae00e76c36f634da36e2d2f23650e6d9a7e423
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-03-06 17:34:23 +0000

    first commit do while loop

---
+++ src/chapter4/AddNumbers.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+/*
+*DO WHILE LOOP:
+* Write a program that allows a user to enter two numbers,
+* and then sums up the two numbers. The user should be able to
+* repeat this action until they indicate they are done.
+ */
+
+public class AddNumbers {
+    public static void main(String args[]){
+
+    }
+}
