33900414e031a015f34a47f10e6d8fcd773177a1
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-03-09 20:47:26 +0000

    added option to enter number of items to scan

--- src/chapter4/Cashier.java
+++ src/chapter4/Cashier.java
@@ -5,9 +5,17 @@ package chapter4;
 * Write a cashier program that will scan a given number of items and tally the cost.
  */
 
+import java.util.Scanner;
+
 public class Cashier {
 
     public static void main(String args[]){
 
+        //Get number of items to scan
+        System.out.println("Enter the number of items you would like to scan:");
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
+
+        double total = 0;
     }
 }
