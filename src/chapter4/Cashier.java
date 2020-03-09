892640cf652f1ddfa7822c6bc167eaa4872a2807
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-03-09 20:44:08 +0000

    First commit For Loop exercise, Cashier program

---
+++ src/chapter4/Cashier.java
@@ -0,0 +1,13 @@
+package chapter4;
+
+/*
+*FOR LOOP:
+* Write a cashier program that will scan a given number of items and tally the cost.
+ */
+
+public class Cashier {
+
+    public static void main(String args[]){
+
+    }
+}
