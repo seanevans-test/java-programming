e526117e899a594c825ab0b70678f7d1c5adcc16
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-03-24 21:50:50 +0000

    added break clause and if else statement for print

--- src/chapter4/LetterSearch.java
+++ src/chapter4/LetterSearch.java
@@ -22,8 +22,16 @@ public class LetterSearch {
         for (int i=0; i<text.length(); i++){
             char currentLetter = text.charAt(i);
             if (currentLetter == 'A'|| currentLetter == 'a'){
-
+                letterFound = true;
+                break;
             }
+        }
+
+        if (letterFound){
+            System.out.println("This text contains the letter 'A'");
+        }
+        else{
+            System.out.println("This text does not contain the letter 'A'");
         }
     }
 }
