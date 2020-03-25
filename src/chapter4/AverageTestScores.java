539884aff54fa456a2ca06c23f7d3ec05fd13765
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-03-25 10:49:24 +0000

    initialised known values

--- src/chapter4/AverageTestScores.java
+++ src/chapter4/AverageTestScores.java
@@ -1,4 +1,17 @@
 package chapter4;
 
+/*
+* NESTED LOOPS:
+* Find the average of each student's test scores
+ */
+
 public class AverageTestScores {
+    public static void main(String args[]){
+
+        //Initialise what we know
+        int numberOfStudents = 24;
+        int numberOfTests = 4;
+
+        //Process all students
+    }
 }
