b7078881721423925ca36339bbc30ce06bac37f9
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-04-05 12:15:00 +0100

    first use of secondary method in class with greetUser

--- src/chapter5/Greetings.java
+++ src/chapter5/Greetings.java
@@ -14,5 +14,10 @@ public class Greetings {
         System.out.println("Enter your name:");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
+        greetUser(name );
+    }
+
+    public static void greetUser(String name){
+        System.out.println("Hi there, " + name );
     }
 }
