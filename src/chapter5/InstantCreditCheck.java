2a7e4175bfe157aa98e11587c4af5271ad0304b5
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-04-05 12:21:58 +0100

    first commit variable scope: credit check

---
+++ src/chapter5/InstantCreditCheck.java
@@ -0,0 +1,15 @@
+package chapter5;
+
+/*
+*Variable Scope
+* Write an 'Instant Credit Check' program that approves anyone who
+* makes more than £25,000 and has a credit score of 700 or better.
+* Reject all others.
+ */
+
+public class InstantCreditCheck {
+
+    public static void main(String args[]){
+
+    }
+}
