0fbcf42ebae23774a00fdd42f745de271dc1e408
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-04-14 18:20:27 +0100

    first use of getlength/setlength and this. keyword

--- src/chapter6/Rectangle.java
+++ src/chapter6/Rectangle.java
@@ -5,11 +5,19 @@ public class Rectangle {
     double length;
     double width;
 
+    double getLength(){
+        return length;
+    }
+
+    void setLength(double length){
+        this.length = length;
+    }
+
     double calculatePerimeter(){
         return (2 * length) + (2 * width);
     }
 
-    double calculateWidth(){
+    double calculateArea(){
         return length * width;
     }
 }
