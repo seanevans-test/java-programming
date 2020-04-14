60d553a713f39a8143d5a268e5c2f7b3a98613f7
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-04-14 20:27:27 +0100

    added method with Rectangle getRoom rather than a primitive datatype in a method

--- src/chapter6/HomeAreaCalcualtorRedo.java
+++ src/chapter6/HomeAreaCalcualtorRedo.java
@@ -1,9 +1,16 @@
 package chapter6;
 
+import java.util.Scanner;
+
 public class HomeAreaCalcualtorRedo {
 
     public static void main(String args[]){
 
+        Rectangle kitchen = getRoom();
+        Rectangle bathroom = getRoom();
+    }
 
+    public Rectangle getRoom(){
+        Scanner scanner = new Scanner(System.in);
     }
 }
