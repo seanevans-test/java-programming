5235fe9a1de842d86b04c91d2173a4389f438e8e
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-04-14 19:38:57 +0100

    first use of a constructor class to determine the shape

--- src/chapter6/Rectangle.java
+++ src/chapter6/Rectangle.java
@@ -5,12 +5,25 @@ public class Rectangle {
     double length;
     double width;
 
+    public Rectangle(){
+        length = 0;
+        width = 0;
+    }
+
     double getLength(){
         return length;
     }
 
     void setLength(double length){
         this.length = length;
+    }
+
+    double getWidth(){
+        return width;
+    }
+
+    void setWidth(double width){
+        this.width = width;
     }
 
     double calculatePerimeter(){
