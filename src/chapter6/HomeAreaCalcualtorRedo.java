722fb19bfaabba6d60ccfe2bfd5fd2ced9c361f9
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-04-14 20:35:01 +0100

    use of return new Rectangle at the end of the method as a clean shortcut

--- src/chapter6/HomeAreaCalcualtorRedo.java
+++ src/chapter6/HomeAreaCalcualtorRedo.java
@@ -12,5 +12,15 @@ public class HomeAreaCalcualtorRedo {
 
     public Rectangle getRoom(){
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the length of your room:");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the width of your room:");
+        double width = scanner.nextDouble();
+
+        scanner.close();
+
+        return new Rectangle(length, width);
     }
 }
