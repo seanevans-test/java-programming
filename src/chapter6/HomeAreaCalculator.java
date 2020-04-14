383d86381fd0234e6cd4031e04d9ac968ebc165d
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-04-14 20:09:31 +0100

    added second rectangle in another form and made equation to add together with printout

--- src/chapter6/HomeAreaCalculator.java
+++ src/chapter6/HomeAreaCalculator.java
@@ -18,5 +18,18 @@ public class HomeAreaCalculator {
         room1.setWidth(25);
         room1.setLength(50);
         double areaOfRoom1 = room1.calculateArea();
+
+        /************************
+        * 2nd Rectangle
+         ***********************/
+
+        //create instance of rectangle class
+        Rectangle room2 = new Rectangle(30,75);
+        double areaOfRoom2 = room2.calculateArea();
+
+        double totalArea = areaOfRoom1 + areaOfRoom2;
+
+        System.out.println("Area of both rooms: " + totalArea);
+
     }
 }
