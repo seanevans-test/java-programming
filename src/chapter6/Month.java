29cc7d1e75fffe4c3d9c493be106b9371a6035a8
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-05-05 20:23:47 +0100

    first use of 2 methods using same context 'getMonth' but with different parameters String/int

--- src/chapter6/Month.java
+++ src/chapter6/Month.java
@@ -1,4 +1,12 @@
 package chapter6;
 
 public class Month {
+
+    public static String getMonth(int month)[
+            return "";
+    ]
+
+    public static int getMonth(String month){
+        return 0;
+    }
 }
