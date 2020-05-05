3ca3bf8c4067de77f42e650a5a7bf7daf7f847ca
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-05-05 21:43:33 +0100

    set out methods for calculations

--- src/Exercises/PhoneBill.java
+++ src/Exercises/PhoneBill.java
@@ -13,4 +13,18 @@ public class PhoneBill {
      allottedMinutes = 600;
      minutesUsed = 600;
     }
+
+    public PhoneBill(int id){
+        this.id = id;
+        baseCost = 29.99;
+        allottedMinutes = 600;
+        minutesUsed = 600;
+    }
+
+    public PhoneBill(int id, double baseCost, int allottedMinutes, int minutesUsed){
+        this.id = id;
+        this.baseCost = baseCost;
+        this.allottedMinutes = allottedMinutes;
+        this.minutesUsed = minutesUsed;
+    }
 }
