3a60b570a1fca9a2eb9c91fd8e5095dc483ea1c0
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-05-05 20:30:58 +0100

    first use of switch and case key words to allow multiple entry options to cover all months

--- src/chapter6/Month.java
+++ src/chapter6/Month.java
@@ -2,9 +2,26 @@ package chapter6;
 
 public class Month {
 
-    public static String getMonth(int month)[
-            return "";
-    ]
+    public static String getMonth(int month){
+            switch(month){
+                case 1: return "January";
+                case 2: return "February";
+                case 3: return "March";
+                case 4: return "April";
+                case 5: return "May";
+                case 6: return "June";
+                case 7: return "July";
+                case 8: return "August";
+                case 9: return "September";
+                case 10: return "October";
+                case 11: return "November";
+                case 12: return "December";
+                default: return "Invalid month. Please enter a value between 1 and 12"
+
+
+
+            }
+    }
 
     public static int getMonth(String month){
         return 0;
