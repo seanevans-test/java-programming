34f2ad33166fc12d3a87a3fe0c30fd37ecf0458b
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-05-18 19:43:43 +0100

    use of a 'final' constant variable in all upper case, replacing magic number

--- src/chapter7/LotteryTicket.java
+++ src/chapter7/LotteryTicket.java
@@ -2,12 +2,14 @@ package chapter7;
 
 public class LotteryTicket {
 
+    private static final int LENGTH = 6;
+
     public static void main(String[] args){
 
     }
 
     public static int[] generateNumbers(){
 
-        int[] ticket = new int[6];
+        int[] ticket = new int[LENGTH];
     }
 }
