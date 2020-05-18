26f2705663696a51f06ac4fed038a84382ebfe87
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-05-18 19:57:37 +0100

    added a second method to set out the print method, print over println, printline not needed in this scenario

--- src/chapter7/LotteryTicket.java
+++ src/chapter7/LotteryTicket.java
@@ -9,6 +9,8 @@ public class LotteryTicket {
 
     public static void main(String[] args){
 
+        int[] ticket = generateNumbers();
+        printTicket(ticket);
     }
 
     public static int[] generateNumbers(){
@@ -19,6 +21,14 @@ public class LotteryTicket {
 
         for(int i=0; i<LENGTH; i++){
             ticket[i] = random.nextInt(MAX_TICKET_NUMBER) + 1;
+        }
+
+        return ticket;
+    }
+
+    public static void printTicket(int ticket[]){
+        for(int i=0; i<LENGTH; i++){
+            System.out.print(ticket[i] + " | ");
         }
     }
 }
