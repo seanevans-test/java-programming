04b629a0e0f7b4637a6d84cb4bbab170779545a2
Sean Evans <sean-.-evans@hotmail.co.uk>
2020-05-18 19:49:41 +0100

    use of Random class to generate the lottery ticket numbers with a for loop and a second constant variable for the max ticket number

--- src/chapter7/LotteryTicket.java
+++ src/chapter7/LotteryTicket.java
@@ -1,8 +1,11 @@
 package chapter7;
 
+import java.util.Random;
+
 public class LotteryTicket {
 
     private static final int LENGTH = 6;
+    private static final int MAX_TICKET_NUMBER = 69;
 
     public static void main(String[] args){
 
@@ -11,5 +14,11 @@ public class LotteryTicket {
     public static int[] generateNumbers(){
 
         int[] ticket = new int[LENGTH];
+
+        Random random = new Random();
+
+        for(int i=0; i<LENGTH; i++){
+            ticket[i] = random.nextInt(MAX_TICKET_NUMBER) + 1;
+        }
     }
 }
